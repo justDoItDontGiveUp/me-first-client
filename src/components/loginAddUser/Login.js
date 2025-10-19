615e97a6054ddbb4059267429520d93b0fe1b471
justDoItDontGiveUp <malkiesobel@gmail.com>
2025-10-19 12:04:53 +0300

    Add a blank line for improved code readability in Login.js

--- src/components/loginAddUser/Login.js
+++ src/components/loginAddUser/Login.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import '../../css/Login.css'; 
 
+
 export const Login = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
